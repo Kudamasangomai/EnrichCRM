185604638520493ca68110a1c05e65c7fe2085b3
Kuda <kudam775@gmail.com>
2023-02-16 04:27:08 +0200

    Email Verification v1.0

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,54 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Route Redirects -> redirect to the login page
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');
+
+/*
+|--------------------------------------------------------------------------
+| Email Verification Routes
+|--------------------------------------------------------------------------
+*/
+
+/*
+ route which will tell the the user that they need to verify thier email by 
+ clicking
+link send to their email
+
+*/
+Route::get('/email/verify', function () {  
+
+    return view('auth.verify-email');
+
+})->middleware('auth')->name('verification.notice');
+
+
+/*
+The EmailVerificationRequest is a form request that is included with Laravel
+ This request will automatically take care of validating the request's id and 
+ hash parameters.
+*/
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill(); 
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+// Resending The Verification Email
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+/*
+|--------------------------------------------------------------------------
+| End Email Verification Routes
+|--------------------------------------------------------------------------
+*/
+require __DIR__.'/auth.php';
