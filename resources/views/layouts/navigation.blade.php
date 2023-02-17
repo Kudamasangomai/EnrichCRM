fd090710d59386dbc7f98e88a3b3b839d1eb883f
Kuda <kudam775@gmail.com>
2023-02-17 11:37:22 +0200

    node modules err

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -46,6 +46,15 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-dropdown-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-dropdown-link>
+                </form>
             </div>
 
             <!-- Hamburger -->
